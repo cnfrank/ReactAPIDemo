60a52205e0bd4a70107919892cf9e7954923f9a1
cnfrank <9570075@qq.com>
2024-02-28 15:59:54 +1300

    test pass with local db

---
+++ Data/ReactAPIDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ReactAPIDemo.Models;
+
+namespace ReactAPIDemo.Data
+{
+    public class ReactAPIDemoContext : DbContext
+    {
+        public ReactAPIDemoContext (DbContextOptions<ReactAPIDemoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ReactAPIDemo.Models.SuperHero> SuperHero { get; set; } = default!;
+    }
+}
---
+++ Migrations/20240228025604_init.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReactAPIDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHero",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HeroName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeroAlias = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Powers = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHero", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHero");
+        }
+    }
+}
